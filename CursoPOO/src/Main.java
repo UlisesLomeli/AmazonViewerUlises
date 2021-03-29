1f55aac98f8aafced9770f2271374a8fbd9c4a64
Ulises Lomeli <urenda.ul@gmail.com>
2021-03-29 17:31:46 -0600

    FinalDia29Marzo

---
+++ CursoPOO/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HOLA MUNDO");
+		
+		Car car = new Car("SDF123", new Account("Ulises Lomeli","LOUU97"));
+		car.passenger=4;
+		car.printDataCar();
+		
+	}
+
+}
